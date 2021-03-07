08459178c48677a2d7e497f3e35c3629f7ecf0a3
marcowong <wonghokchi0402@gmail.com>
2021-03-07 19:40:10 +0800

    Added CharAndBoolean.java

---
+++ src/ncku/unclethree/CharAndBoolean.java
@@ -0,0 +1,14 @@
+package ncku.unclethree;
+
+public class CharAndBoolean {
+    public static void main(String[] args)
+    {
+        char valuechar = 'A';
+        char unicodechar = '\u0041';
+        System.out.println(valuechar);
+        System.out.println(unicodechar);
+
+        boolean valuetrue = true;
+        boolean valuefalse = false;
+    }
+}
