2cecf32db942f21da21afa08277db32288d684d6
marcowong <wonghokchi0402@gmail.com>
2021-03-07 19:12:33 +0800

    Added FloatingPointChallenge.java

---
+++ src/ncku/unclethree/FloatingPointChallenge.java
@@ -0,0 +1,9 @@
+package ncku.unclethree;
+
+public class FloatingPointChallenge {
+    public static void main(String[] args){
+        int pounds = 200;
+        double kilogram = (double) pounds * 0.45359237d;
+        System.out.println(kilogram);
+    }
+}
