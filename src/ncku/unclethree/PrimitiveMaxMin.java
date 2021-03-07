63009e293c0d6245149fdd7fc0e7d0c3d58a34bc
marcowong <wonghokchi0402@gmail.com>
2021-03-07 17:12:29 +0800

    PrimitiveMaxMin.java spelling correction

--- src/ncku/unclethree/PrimitiveMaxMin.java
+++ src/ncku/unclethree/PrimitiveMaxMin.java
@@ -14,17 +14,17 @@ public class PrimitiveMaxMin {
         byte maxbytevalue = Byte.MAX_VALUE;
         byte minbytevalue = Byte.MIN_VALUE;
         System.out.println("Max of byte : " + maxbytevalue); //127
-        System.out.println("Mix of byte : " + minbytevalue); //-128
+        System.out.println("Min of byte : " + minbytevalue); //-128
 
         short maxshortvalue = Short.MAX_VALUE;
         short minshortvalue = Short.MIN_VALUE;
         System.out.println("Max of short : " + maxshortvalue); //32767
-        System.out.println("Mix of short : " + minshortvalue); //-32768
+        System.out.println("Min of short : " + minshortvalue); //-32768
 
         long alongvalue = 999L;
         long maxlongvalue = Long.MAX_VALUE;
         long minlongvalue = Long.MIN_VALUE;
         System.out.println("Max of long : " + maxlongvalue); //9223372036854775807
-        System.out.println("Mix of long : " + minlongvalue); //-9223372036854775808
+        System.out.println("Min of long : " + minlongvalue); //-9223372036854775808
     }
 }
