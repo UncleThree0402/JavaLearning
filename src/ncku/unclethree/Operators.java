1c83bbab21592a0e34b944bfb6b27eeb1bf128da
marcowong <wonghokchi0402@gmail.com>
2021-03-08 00:03:03 +0800

    Added Operators.java

---
+++ src/ncku/unclethree/Operators.java
@@ -0,0 +1,18 @@
+package ncku.unclethree;
+
+public class Operators {
+    public static void main(String[] args){
+        float valueintOne = 404f;
+        float valueintTwo = 200f;
+
+        System.out.println("ValueOne + ValueTwo = " + (valueintOne + valueintTwo));
+        System.out.println("ValueOne - ValueTwo = " + (valueintOne - valueintTwo));
+        System.out.println("ValueOne * ValueTwo = " + (valueintOne * valueintTwo));
+        System.out.println("ValueOne / ValueTwo = " + (valueintOne / valueintTwo));
+        System.out.println("ValueOne % ValueTwo = " + (valueintOne % valueintTwo));
+    }
+}
+
+/*
+x = x - y can be x -= y
+ */
