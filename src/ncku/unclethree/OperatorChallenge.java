ebd51793e6bbe97d5cb03d1e4f1450be0ff0a763
marcowong <wonghokchi0402@gmail.com>
2021-03-08 02:08:46 +0800

    Testing auto format

--- src/ncku/unclethree/OperatorChallenge.java
+++ src/ncku/unclethree/OperatorChallenge.java
@@ -1,15 +1,15 @@
 package ncku.unclethree;
 
 public class OperatorChallenge {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double valueDoubleOne = 20d;
         double valueDoubleTwo = 80d;
         double valueDoubleModulus = ((valueDoubleOne + valueDoubleTwo) * 100d) % 40d;
-        System.out.println("Remainder: "+valueDoubleModulus);
-        boolean valueZero = (valueDoubleModulus==0)?true:false;
+        System.out.println("Remainder: " + valueDoubleModulus);
+        boolean valueZero = (valueDoubleModulus == 0) ? true : false;
         System.out.println("No remainder ? " + valueZero);
 
-        if(valueZero != true){
+        if (valueZero != true) {
             System.out.println("Got some remainder");
         }
     }
