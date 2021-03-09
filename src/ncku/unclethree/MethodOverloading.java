38fc25c6b6f6fa9a5910e862f3a634acde867db9
marcowong <wonghokchi0402@gmail.com>
2021-03-09 12:49:50 +0800

    Added MethodOverloading.java Ver1.1

--- src/ncku/unclethree/MethodOverloading.java
+++ src/ncku/unclethree/MethodOverloading.java
@@ -1,7 +1,5 @@
 package ncku.unclethree;
 
-import java.text.DecimalFormat;
-
 public class MethodOverloading {
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
         if (feet < 0 || inches < 0 || inches > 12) {
@@ -10,12 +8,12 @@ public class MethodOverloading {
             return feet * 30.48 + inches * 2.54;
         }
     }
-    public static double calcFeetAndInchesToCentimeters(double inches){
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
         if (inches < 0) {
             return -1;
-        }
-        else{
-             return calcFeetAndInchesToCentimeters((int)inches/12 , (int) inches%12);
+        } else {
+            return calcFeetAndInchesToCentimeters((int) (inches / 12), inches % 12);
         }
     }
 }
